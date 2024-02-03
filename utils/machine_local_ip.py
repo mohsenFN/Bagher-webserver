838278bf0eb88228ccda576a1e3cfbe950300d48
N-1 <maybeevmo@gmail.com>
2024-02-03 05:27:19 -0500

    smaller function are going to be in this dir

---
+++ utils/machine_local_ip.py
@@ -0,0 +1,18 @@
+'''
+The function in this script is used to get machine's local IPv4 address
+'''
+
+import socket
+
+def get_local_ipv4():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    # 11228 TATALOO
+    # This connection doesn't have to be reachable
+    s.connect(('1.1.22.8', 80))
+
+    local_ipv4 = s.getsockname()[0]
+
+    s.close()
+
+    return local_ipv4
