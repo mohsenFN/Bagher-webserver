47f7a8c9ebcda56bbac8e8e5c2e093e0150dce2a
N-1 <maybeevmo@gmail.com>
2024-02-05 06:47:46 -0500

    Add function to use custom storage address

--- utils/storage_init.py
+++ utils/storage_init.py
@@ -7,3 +7,8 @@ def init():
     if not os.path.exists(storage_path):
         os.makedirs(storage_path)
     return 'storage', storage_path
+
+
+# Usage example --> custom_storage_dir('/tmp/project', 'storage')
+def custom_storage_dir(address, dir_name):
+    return dir_name, address+dir_name
