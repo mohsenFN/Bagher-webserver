88c9b786378735661646d2c115033a942bd5cb20
N-1 <maybeevmo@gmail.com>
2024-02-05 06:40:03 -0500

    Renamed to storage_init

--- utils/storage_manager.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def make_storage_dir():
-    cwd = os.getcwd()
-    storage_path = cwd+'/storage'
-    # make directory if not exists
-    if not os.path.exists(storage_path):
-        os.makedirs(storage_path)
